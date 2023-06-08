95e0ce4697b67129dcd47e7eb8fa4131b6aa0704
Branger-timothe <120025868+Branger-timothe@users.noreply.github.com>
2023-06-08 20:58:42 +0200

    i

---
+++ html/script.js
@@ -0,0 +1,17 @@
+function researchByAuthor(){
+    let authorName=document.getElementById("realisateur");
+}
+function researchByDuree(){
+    let dureeMovie=document.getElementById("duree");
+
+}
+function research(){
+    let dureeMovie=document.getElementById("duree");
+    let authorName=document.getElementById("realisateur");
+    if(authorName.length!==0){
+        
+    }
+    if(dureeMovie.length!==0){
+        researchByDuree();
+    }
+}
