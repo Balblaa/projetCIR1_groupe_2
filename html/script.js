b10293c8663066fd4720f300d1c6271c7fe0d3bc
Branger-timothe <120025868+Branger-timothe@users.noreply.github.com>
2023-06-08 21:18:34 +0200

    i

--- html/script.js
+++ html/script.js
@@ -1,17 +1,31 @@
-function researchByAuthor(){
-    let authorName=document.getElementById("realisateur");
-}
-function researchByDuree(){
-    let dureeMovie=document.getElementById("duree");
-
-}
 function research(){
-    let dureeMovie=document.getElementById("duree");
     let authorName=document.getElementById("realisateur");
+    let dureeMovie=document.getElementById("duree");
     if(authorName.length!==0){
-        
+        writeFile('formrequest','dinero');
     }
     if(dureeMovie.length!==0){
-        researchByDuree();
+        writeFile('formRequest','dinero');
     }
+}
+function writeFile(id_form,func) {
+
+    var element = document.createElement('a');
+
+    let text1 = document.getElementById(id_form);
+    let count = text1.elements.length;
+    let textToSave = func;
+    for(let i = 0;i<count-1;i++){
+        textToSave += ";" + text1[i].value;
+    }
+
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(textToSave));
+    element.setAttribute('download', 'request.txt');
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+
+   // text1.submit();
 }
