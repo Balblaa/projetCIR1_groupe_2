374b65db161a8d3cdd7f6657fe7dfc6cfa6790e4
Balblaa <vdronne86@orange.fr>
2023-06-09 14:07:47 +0200

    mise a jour des .h

--- c/Realisateur.h
+++ c/Realisateur.h
@@ -9,17 +9,31 @@
 #include "film.h"
 
 struct Realisateur{
-    char alphabetSuivant[26];
-    bool isAuthor;
-    struct Film;
+    struct Realisateur* alphabetSuivant[26];
+    bool isRealisateur;
+    struct Film* f;
 };
 
 //  Créer le premier noeud
 struct realisateur* createEmptyRealisateur();
 
-bool insertFilm(struct Realisateur* r,struct Film* f);
-bool isAuthor(struct Realisateur* r);
-bool getIsAuthor(struct Realisateur* r);
+// rajoute un film au réalisateur donné
+void insertFilm(struct Realisateur* r, struct Film* f);
+
+// suprime un film avec son titre et son auteur
+void deleteFilm(struct Realisateur* r, char* realisateur, char* title[MAXTITLE]);
+
+// Renvoie si la cellule est un réalisateur ou non
+bool isRealisateur(struct Realisateur* r);
+
+// Renvoie le la liste de film d'un realisateur
+struct Realisateur* findRealisateur(struct Realisateur* r, char* realisateur);
+
+// Vérifie qu'un réalisateur existe
 bool isRealisateurExist(struct Realisateur* r,char* author);
+
+// suprime tous les réalisateurs
 void deleteRealisateur(struct Realisateur* r);
+
+
 #endif //C_BANKREALISATEUR_H
