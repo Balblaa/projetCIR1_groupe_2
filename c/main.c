98ddae597dfedc0f5885875ebe18755ca4d1a029
Balblaa <vdronne86@orange.fr>
2023-06-09 16:00:10 +0200

    correction addFirst film.c

--- c/main.c
+++ c/main.c
@@ -6,12 +6,12 @@
 #include "film.h"
 
 int main() {
-    struct Film* f=createFilm("rasta kekette", "humour", 95);
+    struct Film* f = createFilm("rasta kekette", "humour", 95);
     addFilm(f, "le petit garcon au pyjama rayé", "humour", 150);
     addFilm(f, "le sang de dieu", "action", 69);
     addFilm(f, "alcoolisme", "drame", 78);
     addFilm(f, "les victoire de ssm", "horreur", 199);
-    getFilmByTitle(f,"alcoolisme");
-    printf("\n");
+    printFilm(f);
+    f = deleteFirst(f);
     printFilm(f);
 }
