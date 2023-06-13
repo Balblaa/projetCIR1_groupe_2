4e551bdc107a88e313ebed67f7efcdfddc816193
Clément Brossaud <clement.brossaud@isen-ouest.yncrea.fr>
2023-06-13 11:47:56 +0200

    modif pour afficher

--- js/recherche.js
+++ js/recherche.js
@@ -1,5 +1,4 @@
-function research(event){
-    event.preventDefault();
+function research(){
     //on récupère les éléments du formulaire
     let authorName=document.getElementById("realisateur").value;
     let dureeMovie=document.getElementById("duree").value;
@@ -11,7 +10,6 @@ function research(event){
     if(dureeMovie.length!==0){
         writeFile('research','searchByTime');
     }
-    printMovies();
 }
 // fonction qui permet d'écrire dans un fichier
 function writeFile(id_form,func) {
@@ -120,57 +118,64 @@ function printMovies(){
     let movies = readFile();
     // Division des données des films en un tableau en utilisant le saut de ligne comme séparateur
     let moviesArray = movies.split("\n");
-    // Récupération de la référence à l'élément de tableau HTML avec l'ID "moviesTable"
-    let table = document.getElementById("moviesTable");
-    // Parcours de chaque film dans le tableau moviesArray
-    for(let i = 0; i < moviesArray.length - 1; i++){
-        // Séparation des données du film en utilisant le point-virgule comme séparateur
-        let movie = moviesArray[i].split(";");
+    //afficher le temps d'exécution
+    document.getElementById("temps_exec").innerHTML = "le temps d'exécution est de "+moviesArray[0]/1000+" secondes";
+    if(moviesArray[1]!="bestAuthor"){
+        // Récupération de la référence à l'élément de tableau HTML avec l'ID "moviesTable"
+        let table = document.getElementById("moviesTable");
+        // Parcours de chaque film dans le tableau moviesArray
+        for(let i = 1; i < moviesArray.length - 1; i++){
+            // Séparation des données du film en utilisant le point-virgule comme séparateur
+            let movie = moviesArray[i].split(";");
 
-        // Insertion d'une nouvelle ligne dans le tableau HTML
-        let row = table.insertRow(i+1);
-        
-        // Insertion de deux cellules dans la nouvelle ligne pour afficher les données du film
-        let cell1 = row.insertCell(0);
-        let cell2 = row.insertCell(1);
-        let cell3 = row.insertCell(2);
-        let cell4 = row.insertCell(3);
-        
-        // Remplissage des cellules avec les données du film
-        cell1.innerHTML = movie[0];
-        cell2.innerHTML = movie[1];
-        cell3.innerHTML = movie[2];
-        cell4.innerHTML = movie[3]; 
+            // Insertion d'une nouvelle ligne dans le tableau HTML
+            let row = table.insertRow(i+1);
+            
+            // Insertion de deux cellules dans la nouvelle ligne pour afficher les données du film
+            let cell1 = row.insertCell(0);
+            let cell2 = row.insertCell(1);
+            let cell3 = row.insertCell(2);
+            let cell4 = row.insertCell(3);
+            
+            // Remplissage des cellules avec les données du film
+            cell1.innerHTML = movie[0];
+            cell2.innerHTML = movie[1];
+            cell3.innerHTML = movie[2];
+            cell4.innerHTML = movie[3]; 
+        }
+        //effacer le contenu du fichier results.txt et ready.txt
+        writeFile("deleteFilmotheque", "results.txt");
+        writeFile("deleteFilmotheque", "ready.txt");
+        return moviesArray;
     }
-    //effacer le contenu du fichier results.txt et ready.txt
-    writeFile("deleteFilmotheque", "results.txt");
-    writeFile("deleteFilmotheque", "ready.txt");
-    return moviesArray;
 }
 
 function deleteFiles(){
     writeFile("research", "stopprocess");
 }
 
-function printBestAuthor(){
+function requestBestAuthor(){
     writeFile("research", "searchbestauthor");
+}
+
+function printBestAuthor(){
     let bestAuthor = readFile();
     let info = bestAuthor.split("\n");
 
+    //
+
     document.getElementById("temps_exec").innerHTML = info[0];
 
-    let info_rea = info[1].split(";");
+    let info_rea = info[2].split(";");
     let texte = "Le réalisateur qui a le plus de films dans la filmothèque est " + info_rea[0] + " avec " + info_rea[1] + " films.";
-    document.getElementById("ouai").innerHTML = texte;
+    document.getElementById("best_rea").innerHTML = texte;
 
 }
 
-function insererFilm(){
-    let realisateur = document.getElementById("realisateur").value;
-    let duree = document.getElementById("duree").value;
-    let film = document.getElementById("titre").value;
-    let categorie = document.getElementById("genre").value;
-    let filmString = realisateur + ";" + duree + ";" + film + ";" + categorie + "\n";
-    writeFile('filmString', "ready.txt");
-    return filmString;
+let xhr = new XMLHttpRequest();
+xhr.open("GET", "ready.txt", false);
+xhr.send(null);
+if(xhr.status !== 404){
+    printBestAuthor();
+    printMovies();
 }
